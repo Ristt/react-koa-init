7e52aa926a0b3c8791b10fdc8f0d4efbfa4e2f3f
Ristt <983474780@qq.com>
2019-07-12 18:29:12 +0800

    add

--- .umirc.ts
+++ .umirc.ts
@@ -1,31 +1,41 @@
 import { IConfig } from 'umi-types';
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'react-koa-init',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: { webpackChunkName: true },
+        title: 'react-koa-init',
+        dll: true,
+        locale: {
+          enable: true,
+          default: 'en-US',
+        },
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+        proxy: {
+          '/api': {
+            target: 'http://192.168.1.181:8088/api',
+            changeOrigin: true,
+            pathRewrite: { '/api': '' },
+          },
+        },
       },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
+    ],
   ],
-}
+};
 
 export default config;
---
+++ src/utils/config.ts
@@ -0,0 +1,1 @@
+export const urlAdress = 'http://192.168.1.181:8088/api';
